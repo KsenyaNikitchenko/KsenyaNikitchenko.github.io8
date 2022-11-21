b072e14a121cc4022e3aa41b80503076fb06fea5
KsenyaNikitchenko <113337726+KsenyaNikitchenko@users.noreply.github.com>
2022-11-21 23:54:24 +0300

    Update form.js

--- form.js
+++ form.js
@@ -1,46 +1,69 @@
-const openPopUp=document.getElementById('open_pop-up');
-const closePopUp=document.getElementById('pop-up_close');
-const PopUp=document.getElementById('pop-up');
-openPopUp.addEventListener('click',function(e){
-    e.preventDefault();
-    PopUp.classList.add('active');
-});
-closePopUp.addEventListener('click',()=>{
-    PopUp.classList.remove('active');
-});
-console.log(localStorage);
-
-const form=document.getElementById('form');
-const formFields=form.elements;
-const submitBtn=form.querySelector('[type="submit"]');
-submitBtn.addEventListener('click',cleareStorage);
-function cleareStorage(){
-    localStorage.clear();
-}
-function changeHandler(){
-    if(this.type!=='checkbox'){
-        console.log(this.name,this.value);
-        localStorage.setItem(this.name,this.value);
-    } else{
-        console.log(this.name,this.checked);
-        localStorage.setItem(this.name,this.checked);
+document.addEventListener('DOMContentLoaded', function () {
+    const openPopUp = document.getElementById('open_pop-up');
+    const closePopUp = document.getElementById('pop-up_close');
+    const PopUp = document.getElementById('pop-up');
+    openPopUp.addEventListener('click', () => {
+        PopUp.classList.add('active');
+        history.pushState({ page: 1 }, '', '?form');
+    });
+    closePopUp.addEventListener('click', () => {
+        PopUp.classList.remove('active');
+    });
+    const form = document.getElementById('form');
+    const formFields = form.elements;
+    let json = { name: '', email: '', message: '', checkbox: '' };
+    form.addEventListener('submit', function (e) {
+        e.preventDefault();
+        cleareStorage();
+        fetch('https://formcarry.com/s/bBjYw4bid', {
+            method: 'POST',
+            headers: { 'Content-Type': 'application/json', 'Accept': 'application/json' },
+            body: JSON.stringify(json)
+        })
+            .then(response => {
+                if (response.ok) {
+                    alert("Спасибо за отзыв!");
+                } else{
+                    alert("Не удалось отправить отзыв( Попробуйте снова")
+                }
+            })
+            .catch(error => console.log(error))    
+    });
+    //После отправки формы localeStorage очищается
+    function cleareStorage() {
+        localStorage.clear();
     }
-}
-function chekStorage(){
-    for(let i=0;i<formFields.length;i++){
-        if(formFields[i].type!=='submit'){
-            if(formFields[i].type==='checked'){
-                formFields[i].checked=localStorage.getItem(formFields[i].name);
-            }else{
-                formFields[i].value=localStorage.getItem(formFields[i].name);
-            }
+    //Сохранение полей в localeStorage
+    function saveStorage() {
+        if (this.type !== 'checkbox') {
+            console.log(this.name, this.value);
+            localStorage.setItem(this.name, this.value);
+            json[this.name] = this.value;
+        } else {
+            console.log(this.name, this.checked);
+            localStorage.setItem(this.name, this.checked);
+            json[this.name] = this.checked;
         }
     }
-    attachEvents();
-}
-function attachEvents(){
-    for(let i=0;i<formFields.length;i++){
-        formFields[i].addEventListener('change',changeHandler);
+    //Если есть данные в localeStorage, то происходит заполнение полей формы
+    function chekStorage() {
+        for (let i = 0; i < formFields.length; i++) {
+            if (formFields[i].type !== 'submit') {
+                if (formFields[i].type === 'checked') {
+                    formFields[i].checked = localStorage.getItem(formFields[i].name);
+                    json[this.name] = this.checked;
+                } else {
+                    formFields[i].value = localStorage.getItem(formFields[i].name);
+                    json[this.name] = this.value;
+                }
+            }
+        }
+        attachEvents();
     }
-}
-chekStorage()
+    function attachEvents() {
+        for (let i = 0; i < formFields.length; i++) {
+            formFields[i].addEventListener('change', saveStorage);
+        }
+    }
+    chekStorage();
+});
